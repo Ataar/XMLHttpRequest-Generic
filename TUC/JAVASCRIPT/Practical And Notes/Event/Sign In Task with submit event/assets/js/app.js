d813dc539aad025402bdcb9f252776d5a088d2f1
Mateen Ataar <mateenataar07@gmail.com>
2023-08-29 10:39:23 +0530

    Submit Event -Student task

---
+++ TUC/JAVASCRIPT/Practical And Notes/Event/Sign In Task with submit event/assets/js/app.js
@@ -0,0 +1,61 @@
+
+const sForm = document.getElementById('signInForm');
+
+let email = document.getElementById('email');
+
+let password = document.getElementById('password');
+
+
+let emailFromDb = 'Mateen';
+let passFromDb = 'Ataar';
+// let stuData =[
+//     {
+//         email : 'Mateen@gmail.com', password : 705880
+//     }
+// ]
+
+let onStd = (eve)=>{
+    eve.preventDefault();
+   
+let emailVal = email.value;
+let passVal = password.value;
+
+
+if(emailVal===emailFromDb && passVal===passFromDb)
+{
+    // alert(`Sign In Successfully`)
+    Swal.fire({
+        icon: 'success',
+        title: 'You are logged in Successfully',
+        showConfirmButton: false,
+        timer: 3000
+      })
+        sForm.reset();
+// here reset method is used to reset the values of elements in a form.
+    return true;
+}
+
+
+else
+{
+    // alert(`Invalid Username or Password`)
+    Swal.fire({
+        icon: 'error',
+        // title: 'Oops...',
+        title:'Sorry!',
+        text: 'Invalid Username or Password',
+        timer: 3000
+    })
+        sForm.reset();
+        // here reset method is used to reset the values of elements in a form.
+    return false;
+
+}
+
+
+}
+sForm.addEventListener('submit',onStd)
+
+
+
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,78 @@
+let stdForm = document.getElementById('stdForm');
+let stdCon = document.getElementById('stdCon');
 
-let std = document.getElementById('std-form')
 let fname01 = document.getElementById('fname')
 let lname01 = document.getElementById('lname')
 let email01 = document.getElementById('email')
 let contact01 = document.getElementById('contact')
-const stdContainer = document.getElementById('stdContainer')
 
-
-let stdArray =[]
-
-const template = (arr)=>{
-
-
-    let result ='';
-
-   arr.forEach((add , i)=>
-    {
-        result+=   `
-        <tr>
-        <td>${i+1}</td>
-        <td>${add.fname}</td>
-        <td>${add.lname}</td>
-        <td>${add.email}</td>
-        <td>${add.contact}</td>
-      </tr>
-        
-        `   
-    })
-
-    stdContainer.innerHTML = result
-
-}
-
-
-
-
-
-
-
-
-
-
-
-let onSubm =(eve)=>
+let stdArray =[
 {
-    eve.preventDefault();
-    // console.log('Form Submitted')
+   fname :'Mateen',
+   lname :'Ataar',
+   email  :'mateen@gmail.com',
+   contact:9734468690
+},
+{
 
-     // To stop the sumbit event behaviour preventDefault() is used.
+  fname :'Sachin',
+   lname :'Thakur',
+   email  :'Sachin@gmail.com',
+   contact:893523523
+
+}
+]
+
+let tempalting=(arr)=>{
 
 
+  
+let result ='';
+arr.forEach((add,i)=>{
 
-    let stu01 =
-    {
-       fname :fname01.value,
-       lname :lname01.value,
-       email:email01.value,
-       contact:contact01.value
+  result+= `
+  
+<tr>
+      <td>${i+1}</td>
+      <td>${add.fname}</td>
+      <td>${add.lname}</td>
+      <td>${add.email}</td>
+      <td>${add.contact}</td>
+</tr>
+  
+  `
+})
+stdCon.innerHTML = result;
 
-    }
-     
-    stdArray.push(stu01)
-
-    template(stdArray)
-    // console.log(stdArray);   
-    eve.target.reset()
 }
 
+tempalting(stdArray)
+
+const onStd =(eve)=>
+{
+  eve.preventDefault();
+
+let stdObj =
+  {
+    fname : fname01.value,
+    lname : lname01.value,
+    email : email01.value,
+    contact :contact01.value
+  }
+// console.log(stdObj)
+   stdArray.push(stdObj)
+   eve.target.reset();
+   tempalting(stdArray)
+}
+
+stdForm.addEventListener('submit',onStd)
 
 
 
-std.addEventListener('submit',onSubm)
 
-// Note:- submit event has a default behaviour that if the submit event is triggerd it refresh a page fastly.  
+// Note:
+// 1) hame templating table ko hamesha Function Ke ander lena hota hai.
+
+// 2)form contorl me se data lene ke liye Hum '.value' ye property use karte hain.
+// 3) tempalating ka use nahi karna hai kyoun ke templating sabhi arr ke ko ittrate kar ke 
+  // data or tr remove kar ke wahi data or tr dobara store kar raha hai.
