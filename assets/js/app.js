4afd9134cab4d573620eba7626241de10c0e7419
Mateen <mateenataar260@gmail.com>
2024-08-10 00:10:18 +0530

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,52 @@
 
-let cl = console.log;
-
-let pass = document.getElementById('password')
-
-let data = document.getElementById('data')
 
 
-const onClick= () =>{
-    if(pass.type==='password')
-        {
-            pass.type='text';
-            
-            data.classList.replace('fa-eye','fa-eye-slash')
-        }
-        
-        else
-        {
-            pass.type='password';
-            data.classList.replace('fa-eye-slash','fa-eye')
-        }
-        
-}
 
-data.addEventListener('click',onClick)
+let password = document.getElementById('password');
+let data = document.getElementById('data');
+
+const onPassword = () => {
+    const isPasswordVisible = password.type === 'password';
+    password.type = isPasswordVisible ? 'text' : 'password';  // here ternary operator is used as if else
+    data.classList.toggle('fa-eye-slash', isPasswordVisible); // isPasswordVisible contain 'text'
+    data.classList.toggle('fa-eye', !isPasswordVisible);       // !isPasswordVisible contain 'password'
+};
+
+data.addEventListener('click', onPassword);
+
+
+
+
+
+// *********************************************************************************************************************
+
+
+
+
+
+// let password = document.getElementById('password');
+// let data = document.getElementById('data');
+
+
+// const onPassword=()=>{
+  
+//     if(password.type==='password')
+//     {
+//         password.type='text';
+//         data.classList.add('fa-eye-slash')
+//         data.classList.remove('fa-eye')
+//     }
+//     else
+//     {
+//         password.type='password';
+//         data.classList.add('fa-eye')
+//         data.classList.remove('fa-eye-slash')
+// }
+
+// }
+
+
+// data.addEventListener('click',onPassword);
+
+
+
