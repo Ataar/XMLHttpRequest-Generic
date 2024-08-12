918f8d367272cc0a6fbaf775af7c74977e0ddea1
Mateen <mateenataar260@gmail.com>
2024-08-12 11:49:37 +0530

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,66 +1,117 @@
+let cl = console.log;
 
-let password = document.getElementById('password') // input element has been selected by it's id  
+const blogs = [
+    {
+        title: 'Impotrance Of Agriculture',
+        content: `The uptake of artificial intelligence (AI) in business is accelerating at pace. Through business acquisitions, in-house AI development and the creation of new AI brands, companies have access to a multitude `,
+         images:'assets/images/Ai photo.webp'
+         
+    },
+    {
+        title: "Today's Technology",
+        content: `Modern technology has paved the way for multi-functional devices like the smartwatch and the smartphone. Computers are increasingly faster, more portable, and higher-powered than ever before.`,
+        images: './assets/images/426e09d1704e8e8c10f82df8eb3905ca.jpg'
+    },
 
-let data = document.getElementById('data')    // i element has been selected by it's id
-
-
-const onPassword = ()=>{
-    // console.log('Clicked')
-    const ispasswordVisible = password.type==='password'; // it checks that if the type and value is same then it return true value.
-
-    password.type = ispasswordVisible ? 'text' : 'password'; // it checks if ispasswordVisible is true so set the password type to 'text'
-   //  if it is flase then set the password type to 'password.
-
-
-
+      {title: 'The Healthy Black Coffee',
+      content: `Coffee is a morning staple that many people drink to give them the energy to get through the day. Whether you head to Starbucks or Dunkin' cup of joe or make it at home black coffee has several health benefits.`,
+      images:'./assets/images/coffee-cup-coffee-beans-aroma-wallpaper-preview.jpg'
+     },
    
-   data.classList.toggle('fa-eye-slash',ispasswordVisible); 
-//    This line toggles the class 'fa-eye-slash' on the element data.
-// if ispasswordVisible is true , the class 'fa-eye-slash' will be added to data
+    {
+        title: 'Jack and the Beanstalk',
+        content: `Jack is a poor boy who trades his cow for magic beans. The beans grow into a giant beanstalk reaching up to the sky. Jack climbs the beanstalk and discovers a giant's castle with treasures.`,
+        images: './assets/images/jack-beanstalk.jpg'
+    },
+     
+  
+    {
+        title: 'The King Of Fruite',
+        content: `Mango is the King of fruits whereas  Litchi is the Queen. This very sweet, pulpy aromatic, and musky is also loaded with many health benefits like boosting the immune system, regulating blood health.`,
+        images: './assets/images/fresh-fruit-salad-wooden-table-healthy-delicious-generated-by-artificial-intelligence_188544-125262.avif" alt="The King Of Fruite"'
+     },
+
+     {
+        title:'cinnamon-hot-chocolate-glasses',
+        content:`Cinnamon Hot Chocolate is a wholesome but utterly luxurious healthy hot cocoa drink. Sweetened naturally using dates and with a hint of spice this healthy hot chocolate.`,
+        images:'./assets/images/cinnamon-hot-chocolate-glasses.jpg'
+
+     }
 
 
+    
+];
 
+function createBlog(blog,blog2,blog3) {
+    setTimeout(() => {
+        blogs.push(blog,blog2,blog3);
+        PNotify.success({
+            title: `<span class='color' style='display: flex; align-items: center;'>
+                     Success
+                     <img class='imG' src='assets/images/boy-removebg-preview.png' alt='Success Icon' >
+                     </span>`,
+                     titleTrusted: true,
+                     text: `<span style="color: white;"><strong class='text'>Blog is created successfully</strong></span>`,
+                     textTrusted: true,
+                     maxTextHeight: null,
+                     delay: 200 // Adjust the delay as needed
+                    });
+                    getBlog(blogs);
+    }, 1000);
+}
 
-
-    data.classList.toggle('fa-eye',!ispasswordVisible);
-    // This line toggles the class 'fa-eye' on the element data.
-    // if ispasswordVisible is false ,  the class 'fa-eye' will be added to data
+function getBlog(arr) {
+    setTimeout(() => {
+        cl(arr);
+        temp(arr); // Pass the correct array dc
+    }, 1500);
+        
+      
 
 }
 
+createBlog(
+    {
+        title: 'The Ugly Duckling',
+        content: `A mother duck's eggs hatch and among the ducklings, one is larger and different from the rest. The mother duck is disappointed, but as time passes, the ugly duckling grows into a beautiful swan.`,
+        images: './assets/images/ugly duck.jpg'
+    },
+   {
+    title: 'jack-th-giant-slayer-poster',
+    content: `A Lion was once caught in the toils of a hunter's net. Unable to free himself, he lay roaring for help. A little Mouse came upon him, and, seeing the plight of the Lion, gnawed the net to pieces and set him free.`,
+    images: './assets/images/jack-th-giant-slayer-poster.jpg'
+},
 
-data.addEventListener('click',onPassword) 
-
-
-// *********************************************************************************************************************
-
-
-
-
-
-// let password = document.getElementById('password');
-// let data = document.getElementById('data');
-
-
-// const onPassword=()=>{
-  
-//     if(password.type==='password')
-//     {
-//         password.type='text';
-//         data.classList.add('fa-eye-slash')
-//         data.classList.remove('fa-eye')
-//     }
-//     else
-//     {
-//         password.type='password';
-//         data.classList.add('fa-eye')
-//         data.classList.remove('fa-eye-slash')
-// }
-
-// }
+ {
+    title: 'Yummy Corp feeds',
+    content: `In 2016, Mario Suntanu was a general partner at venture capital firm SMDV, but he saw a shift in the food and beverages industry. “Restaurants would not survive if they only have offline services.`,
+    images: './assets/images/Yummy-Corp-RD-Team.png'
+ },
 
 
-// data.addEventListener('click',onPassword);
+);
+
+let data = document.getElementById('data');
 
 
+const temp = () => {
+    let result = '';
+   blogs.forEach((add) => {
 
+
+        result += `
+            <div class="col-md-4 mb-0">
+                <div class="card h-4">
+                <div class="card-body">
+                <h5 class="card-title text-center p-3">${add.title}</h5>
+                <img src="${add.images}" class="card-img-top" alt="Card image">
+                <p class="card-text">${add.content}</p>
+                    </div>
+                </div>
+            </div>
+            
+        `;
+        
+    });
+    data.innerHTML = result;
+};
